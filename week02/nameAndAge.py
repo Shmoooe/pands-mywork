2a5704e632004ab15f469b1d4ca60d1cef2a8954
Joanna Kelly <joannabkelly@gmail.com>
2024-02-05 11:10:17 +0000

    week 02

---
+++ week02/nameAndAge.py
@@ -0,0 +1,18 @@
+#nameAndAge.py
+#to print the name and age of the person inputting their data
+#author: Joanna Kelly
+
+
+name = input("Enter your name: ")
+age = input('What is your age? ')
+print(f'Hello {name}, your age is {age}.')
+
+
+'''
+answer = 21-4
+print(answer)
+'''
+'''
+answer = 2==3
+print(answer)
+'''
