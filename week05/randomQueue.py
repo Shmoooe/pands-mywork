760e1f41e4add8e8afb548cde12cbd33d7235f06
Joanna Kelly <joannabkelly@gmail.com>
2024-02-25 13:47:33 +0000

    LUNCH

---
+++ week05/randomQueue.py
@@ -0,0 +1,19 @@
+# This program puts 10 random numbers into a list named queue.
+# It then outputs all the values in the queue,
+# then prints each number one at a time and shows the current numbers still in the queue.
+# Author: Joanna K
+
+
+import random
+
+queue = []
+
+for n in range(0,10):
+    queue.append(random.randint(0,100))
+    print(f"Queue is {queue}")
+
+while len(queue) != 0:
+    currentNumber = queue.pop(0)
+    print(f"Current number is {currentNumber} and the queue is {queue}")
+
+print("The queue is now empty")
